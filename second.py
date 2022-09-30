8ad77ef94e6e545e51be55b9cff4fad5fe4b73cf
ChumakovaLiza940 <113980408+ChumakovaLiza940@users.noreply.github.com>
2022-09-30 10:30:37 +0300

    Add files via upload

---
+++ second.py
@@ -0,0 +1,9 @@
+#номер 1
+import math
+from re import S
+
+X = 14.26
+Y = -1.22
+Z = 3.5*10**(-2)
+s=2*math.cos(X-2/3)/(1/2+math.sin(Y)*math.sin(Y))*(1+Z**2/(3-Z**2/5))
+print(S)
---
+++ thir.py
@@ -0,0 +1,4 @@
+9 = int(input()) 
+10 = int(input())
+11 = int(input())
+
