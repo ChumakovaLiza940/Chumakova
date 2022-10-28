58936451fda4b34dd0501c3eb0b905cdb12cc999
ChumakovaLiza940 <113980408+ChumakovaLiza940@users.noreply.github.com>
2022-10-28 20:50:37 +0300

    Add files via upload

---
+++ Занятие 3/task 6.py
@@ -0,0 +1,8 @@
+a = int(input("Введите a: "))
+b = int(input("Введите b: "))
+c = int(input("Введите c: "))
+d = int(input("Введите d: "))
+if (a+b+c+d) % 2 == 0:
+    print('YES')
+else:
+    print('NO')
